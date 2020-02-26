6b5ee20ad00637c1d5fd2cdcd43ed091df6466d3
ymcidence <yuming.shen@inceptioniai.org>
2020-02-26 16:39:33 +0400

    finalized

--- run_tbh.py
+++ run_tbh.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
 from train import tbh_train
 
-tbh_train.train('cifar10', 32, 512, 256)
+tbh_train.train('cifar10', 32, 512, 400)
